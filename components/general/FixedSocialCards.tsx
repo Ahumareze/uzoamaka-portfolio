05a706873e0a858ca282b095f64e36de8339a2ea
Ahumareze <ahumarezeifeanyi001@gmail.com>
2024-11-26 23:21:16 +0100

    restructured arrangement

--- components/general/FixedSocialCards.tsx
+++ components/general/FixedSocialCards.tsx
@@ -7,9 +7,9 @@ import { TbMail } from "react-icons/tb"
 export default function FixedSocialCards(){
     return(
         <div className="fixed top-[30%] left-0 z-[1000]">
-            <Link href={'https://substack.com/for-bloggers'} target="_blank">
+            <Link href={'https://www.instagram.com/uzoamaka_a?igsh=OGxjdXJkbnU5ZWhy'} target="_blank">
                 <div className="h-[58px] w-[58px] flex items-center justify-center cursor-pointer bg-primary-white text-primary-black">
-                    <SiSubstack size={22} />
+                    <RiInstagramFill size={25} />
                 </div>
             </Link>
             <Link href={'https://x.com/uzoamaka_a1?s=21&t=Kf4Ot9_zpktg2pxUHVIIug'} target="_blank">
@@ -17,14 +17,9 @@ export default function FixedSocialCards(){
                     <FaXTwitter size={22}  />
                 </div>
             </Link>
-            <Link href={'https://www.instagram.com/uzoamaka_a?igsh=OGxjdXJkbnU5ZWhy'} target="_blank">
+            <Link href={'https://substack.com/for-bloggers'} target="_blank">
                 <div className="h-[58px] w-[58px] flex items-center justify-center cursor-pointer bg-primary-white text-primary-black">
-                    <RiInstagramFill size={25} />
-                </div>
-            </Link>
-            <Link href={'mailto:hellouzoamaka@gmail.com'} target="_blank">
-                <div className="h-[58px] w-[58px] flex items-center justify-center cursor-pointer bg-primary-white text-primary-black">
-                    <TbMail size={22} />
+                    <SiSubstack size={22} />
                 </div>
             </Link>
             <Link href={'https://www.linkedin.com/feed/'} target="_blank">
@@ -32,6 +27,12 @@ export default function FixedSocialCards(){
                     <FaLinkedin size={22} />
                 </div>
             </Link>
+            <Link href={'mailto:hellouzoamaka@gmail.com'} target="_blank">
+                <div className="h-[58px] w-[58px] flex items-center justify-center cursor-pointer bg-primary-white text-primary-black">
+                    <TbMail size={22} />
+                </div>
+            </Link>
+            
         </div>
     )
 }
