ef35d062e7c748d6e2066f7e923bee09c4f4427b
Ahumareze <ahumarezeifeanyi001@gmail.com>
2024-12-11 23:43:29 +0100

    added hero section multi images

--- app/components/hero/HeroBackdrop.tsx
+++ app/components/hero/HeroBackdrop.tsx
@@ -6,7 +6,8 @@ import hero3 from '@/assets/images/hero-images/hero-3.png';
 import hero4 from '@/assets/images/hero-images/hero-4.png';
 import hero5 from '@/assets/images/hero-images/hero-5.png';
 import hero6 from '@/assets/images/hero-images/hero-6.png';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "motion/react"
 
 const images : StaticImageData[] = [
     hero1,
@@ -20,13 +21,36 @@ const images : StaticImageData[] = [
 export default function HeroBackdrop(){
     const [currentIndex, setCurrentIndex] = useState<number>(0);
 
+    useEffect(() => {
+        console.log(currentIndex)
+    }, [currentIndex])
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevCount) => (prevCount === 5 ? 0 : prevCount + 1));
+        }, 4000);
+    
+        return () => clearInterval(interval); // Cleanup on unmount
+    }, []);
+
     return(
-        <div className='h-screen w-full bg-[#d6c7c4] bg-red-500'>
-            <Image
-                className='h-full w-full object-cover'
-                src={images[currentIndex]}
-                alt='Banner images'
-            />
+        <div className='h-screen w-full bg-[#d6c7c4] relative'>
+            <AnimatePresence>
+            <motion.div
+                key={currentIndex}
+                initial={{ opacity: 0.3 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 1 }}
+                className="h-screen w-full absolute top-0 left-0"
+            >
+                <Image
+                    className='h-full w-full object-cover'
+                    src={images[currentIndex]}
+                    alt='Banner images'
+                />
+            </motion.div>
+            </AnimatePresence>
         </div>
     )
 }
