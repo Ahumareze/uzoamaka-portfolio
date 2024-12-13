e99d22f5d2c782e39f7aa7d7ea9f73d02b5fb2fb
Ahumareze <ahumarezeifeanyi001@gmail.com>
2024-12-13 21:26:06 +0100

    added biography section

--- app/components/biography/Biography.tsx
+++ app/components/biography/Biography.tsx
@@ -55,34 +55,34 @@ export default function Biography(){
                 <div>
                     <div className='pt-5 px-10 h-screen' id='earlyLife'>
                         <h1 className='font-gelica text-5xl font-bold text-[#444444]'>Early Life</h1>
-                        <div className='space-y-7 mt-5 text-base md:text-lg text-[#222229] font-eculid' data-aos="fade-up">
-                            <p>Uzoamaka Aniunoh was born on August 25, 1990, in Lagos, Nigeria. Her early life was marked by a strong emphasis on education and the arts, with her parents providing unwavering support. Born into a family that valued creative expression, Uzoamaka's innate talent and passion for acting were nurtured from a young age.</p>
+                        <div className='space-y-7 mt-5 text-base md:text-lg text-[#444444] font-eculid' data-aos="fade-up">
+                            <p>Uzoamaka Aniunoh was born and raised in Onitsha, <span className='font-medium'>Nigeria</span>. Her early life and upbringing played a significant role in shaping her creative pursuits. Uzoamaka’s educational background laid the foundation for her future success. She pursued higher education at the <span className='font-medium'>University of Nigeria</span>, where she earned a degree in <span className='font-medium'>English and History.</span></p>
 
-                            <p>Her family relocated to the United States during her childhood, exposing her to diverse cultural experiences that would later influence her acting career. Uzoamaka attended high school in the US before proceeding to study at the prestigious New York Film Academy, laying the groundwork for her professional acting career.</p>
+                            <p>Aniunoh further honed her writing skills by obtaining a Master's degree in <span className='font-medium'>Creative Writing</span> from the <span className='font-medium'>University of Birmingham.</span> This postgraduate degree helped her refine her writing style, explore different genres, and develop a unique voice. Her educational journey not only equipped her with the necessary skills but also fostered a deep appreciation for the creative arts.</p>
 
-                            <p>Growing up, Uzoamaka drew inspiration from renowned Nigerian actresses like Genevieve Nnaji, Omotola Jalade-Ekeinde, and Joke Silva. These screen icons motivated her to pursue her dreams in the entertainment industry. Her formative years were characterized by dedication, perseverance, and a burning passion for storytelling, shaping her unique perspective and paving the way for her rise to prominence in the Nigerian film industry.</p>
+                            <p>Uzoamaka’s academic achievements and early life experiences paved the way for her future success as a <span className='font-medium'>writer, actress, and director.</span> Her strong educational foundation and passion for creative pursuits have been instrumental in shaping her career.</p>
                         </div>
                     </div>
                     {/* herstory */}
                     <div className='w-full h-screen bg-[#F5EDEB] rounded-t-[60px] border-8 border-white p-10 relative' id='herStory'>
                         <h1 className='font-gelica text-5xl font-bold text-[#444444]'>From Writing to Acting </h1>
-                        <div className='space-y-7 mt-5 text-base md:text-lg text-[#222229] font-eculid'>
-                            <p>Uzoamaka Aniunoh was born on August 25, 1990, in Lagos, Nigeria. Her early life was marked by a strong emphasis on education and the arts, with her parents providing unwavering support. Born into a family that valued creative expression, Uzoamaka's innate talent and passion for acting were nurtured from a young age.</p>
+                        <div className='space-y-7 mt-5 text-base md:text-lg text-[#444444] font-eculid'>
+                            <p>Aniunoh's creative journey began as a published fiction writer. Her work has been featured in various literary platforms, including <span className='font-medium'>Brittle Paper,</span> which published her stories "Tea Glass: An African Story" and "Mama Junior and Rush Green." An excerpt from her work "Balcony" was edited and published by renowned Nigerian writer Chimamanda Ngozi Adichie.</p>
 
-                            <p>Her family relocated to the United States during her childhood, exposing her to diverse cultural experiences that would later influence her acting career. Uzoamaka attended high school in the US before proceeding to study at the prestigious New York Film Academy, laying the groundwork for her professional acting career.</p>
+                            <p>Aniunoh's transition to acting began in 2017 when she attended open auditions for <span className='font-medium'>MTV Shuga</span> and landed the role of Cynthia. She went on to star in the series from 2018 to 2019 and reprised her role in the 2020 special edition mini-series <span className='font-medium'>"MTV Shuga Alone Together."</span> This breakthrough role marked the beginning of her successful acting career, showcasing her talent and versatility as a performer.</p>
 
-                            <p>Growing up, Uzoamaka drew inspiration from renowned Nigerian actresses like Genevieve Nnaji, Omotola Jalade-Ekeinde, and Joke Silva. These screen icons motivated her to pursue her dreams in the entertainment industry. Her formative years were characterized by dedication, perseverance, and a burning passion for storytelling, shaping her unique perspective and paving the way for her rise to prominence in the Nigerian film industry.</p>
+                            <p>As Aniunoh's acting career progressed, she took on more challenging roles, collaborating with prominent directors and actors in the Nigerian film industry. Her performances have been met with critical acclaim, earning her numerous award nominations and recognition within the industry.</p>
                         </div>
                     </div>
                     {/* wins */}
                     <div className='w-full h-screen bg-[#EEE1DD] rounded-t-[60px] border-8 border-white p-10 relative' id='wins'>
                         <h1 className='font-gelica text-5xl font-bold text-[#444444]'>Wins, Directing...</h1>
-                        <div className='space-y-7 mt-5 text-base md:text-lg text-[#222229] font-eculid'>
-                            <p>Uzoamaka Aniunoh was born on August 25, 1990, in Lagos, Nigeria. Her early life was marked by a strong emphasis on education and the arts, with her parents providing unwavering support. Born into a family that valued creative expression, Uzoamaka's innate talent and passion for acting were nurtured from a young age.</p>
+                        <div className='space-y-7 mt-5 text-base md:text-lg text-[#444444] font-eculid'>
+                            <p>Aniunoh's acting career has been marked by notable successes. She received the <span className='font-medium'>Kim Renders Memorial Award</span> for Outstanding Performance from the <span className='font-medium'>Reelout Film Festival</span> in 2021 for her role in the LGBT love story "Ife."</p>
 
-                            <p>Her family relocated to the United States during her childhood, exposing her to diverse cultural experiences that would later influence her acting career. Uzoamaka attended high school in the US before proceeding to study at the prestigious New York Film Academy, laying the groundwork for her professional acting career.</p>
+                            <p>In 2022, Aniunoh starred in the Nigerian thriller <span className='font-medium'>"Diiche,"</span> Showmax's first Nigerian original limited series, which received seven nominations at the <span className='font-medium'>2023 Africa Magic Viewers' Choice Awards.</span> Her performance in the 2024 thriller <span className='font-medium'>"The Weekend"</span> earned her a Best Actress nomination at the 20th edition of the Africa Movie Academy Awards. These nominations and awards demonstrate her growth as an actress and her ability to excel in a variety of roles.</p>
 
-                            <p>Growing up, Uzoamaka drew inspiration from renowned Nigerian actresses like Genevieve Nnaji, Omotola Jalade-Ekeinde, and Joke Silva. These screen icons motivated her to pursue her dreams in the entertainment industry. Her formative years were characterized by dedication, perseverance, and a burning passion for storytelling, shaping her unique perspective and paving the way for her rise to prominence in the Nigerian film industry.</p>
+                            <p>In addition to her acting accomplishments, Aniunoh has also ventured into directing. She made her directorial debut in 2023 with the short film <span className='font-medium'>"Love Language,"</span> which she also wrote and starred in.  As Aniunoh continues to grow and evolve as a creative professional, her future projects are eagerly anticipated by fans and industry insiders alike.</p>
                         </div>
                     </div>
                 </div>
