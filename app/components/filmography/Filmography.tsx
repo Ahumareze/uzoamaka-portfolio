7771464a9445ad5322fd140917ecc253726e53a9
Ahumareze <ahumarezeifeanyi001@gmail.com>
2024-12-28 14:21:13 +0100

    added posters to filmography table

--- app/components/filmography/Filmography.tsx
+++ app/components/filmography/Filmography.tsx
@@ -1,19 +1,61 @@
 import { MOVIES } from "@/constants/moviesListing";
 import Image, { StaticImageData } from "next/image";
-import Link from "next/link";
 import { useState } from "react";
-import { GoArrowUpRight } from "react-icons/go";
 import { MovieItem } from "./MovieItem";
 import { AnimatePresence, motion } from "motion/react"
 
 import quiteMondayPoster from '@/assets/images/movie-posters/quiteMonday-poster.png';
+import wndc from '@/assets/images/posters/wdce.png';
+import profileImage from '@/assets/images/profileImage.jpeg';
+import night2005 from '@/assets/images/posters/2005Night.jpg';
+import the_weekend from '@/assets/images/posters/the_weekend.jpg';
+import phoenix_fury from '@/assets/images/posters/phoenix_fury.jpg';
+import headless from '@/assets/images/posters/headless.jpg';
+
+/* eslint-disable */
+import love_notes from '@/assets/images/posters/love_notes.JPG';
+import diche from '@/assets/images/posters/diche.JPG';
+import ife from '@/assets/images/posters/ife.JPG';
+/* eslint-enable */
+
+import mami_wata from '@/assets/images/posters/mami_wata.jpg';
+import domi from '@/assets/images/posters/domi.jpg';
+import shapingus from '@/assets/images/posters/shapingus.jpeg';
+import behindthescene from '@/assets/images/posters/behindthescene.jpeg';
+import thirdStreet from '@/assets/images/posters/3rdStreet.jpeg';
+import riona from '@/assets/images/posters/riona.jpg';
+import amanzeandthezipman from '@/assets/images/posters/amanze&thezipman.jpeg';
+import mtvshuga from '@/assets/images/posters/mtvshuga.jpeg';
+
 import ponziPoster from '@/assets/images/movie-posters/ponzi-poster.png';
 import loveLanguagePoster from '@/assets/images/movie-posters/loveLanguage-poster.png';
 
 const movies = [
     quiteMondayPoster,
+    profileImage,
+    shapingus,
+    night2005,
+    wndc,
+    the_weekend,
+    phoenix_fury,
+    headless,
+    love_notes,
+    domi,
+    mami_wata,
+    loveLanguagePoster,
+    diche,
+    profileImage,
     ponziPoster,
-    loveLanguagePoster
+    behindthescene,
+    amanzeandthezipman,
+    ife,
+    riona,
+    mtvshuga,
+    thirdStreet,
+    profileImage,
+    profileImage,
+    profileImage,
+    mtvshuga
 ]
 
 export interface MovieItemProps {
@@ -41,7 +83,7 @@ export default function Filmography(){
                 <p className="max-w-[350px] text-right font-gelica text-[#8E8E8E] text-lg" data-aos="fade-left">'there are years that ask questions and years that answer'~ <span className="text-[#B4B4B4]">zora neale hurston.</span></p>
             </div>
             <div className="w-full flex gap-10 md:gap-20 mt-10 relative">
-                <div className="w-[420px] flex items-center justify-center sticky top-5 h-fit">
+                <div className="w-[420px] flex items-center justify-center sticky top-10 h-fit pt-10">
                     <AnimatePresence>
                         <motion.div 
                             key={selectedIndex}
@@ -53,7 +95,7 @@ export default function Filmography(){
                             <Image
                                 src={movies[selectedIndex]}
                                 alt=''
-                                className="group-hover:scale-105 duration-300 ease-in-out"
+                                className="group-hover:scale-105 duration-300 ease-in-out bg-[#ddd] rounded-md"
                             />
                         </motion.div>
                     </AnimatePresence>
