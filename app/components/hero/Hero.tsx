e2b4b29e9852e6e1ca2e06114eb2ebffa66edcc7
Ahumareze <ahumarezeifeanyi001@gmail.com>
2025-01-14 12:14:23 +0100

    added mobile coursel view

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -22,15 +22,12 @@ export default function Hero(){
     return(
         <section className='h-screen relative'>
             <HeroBackdrop />
-            <div className="h-screen overflow-hidden w-full bg-center bg-cover bg-no-repeat flex gap-10 px-[20px] md:px-[50px] absolute top-0">
+            <div className="h-screen overflow-hidden w-full bg-center bg-cover bg-no-repeat flex md:flex-row flex-col gap-2 md:gap-10 md:px-[50px] pb-5 md:pb-0 absolute top-0">
                 <div className='flex-1 flex items-end h-full overflow-hidden py-[20px]'>
                     <h1 className='text-primary-white text-5xl md:text-7xl lg:text-9xl font-extrabold m-0 p-0 font-gelica' data-aos="fade-up">UZOAMAKA<br />ANIUNOH.</h1>
-                    {/* <LargeText
-                        text='UZOAMAKA'
-                    /> */}
                 </div>
-                <div className='h-screen overflow-hidden w-[200px] relative'>
-                    <div className={`${classes.scroll_container} h-full flex flex-col gap-5`}>
+                <div className='h-[200px] md:h-screen overflow-hidden w-fukk md:w-[200px] relative'>
+                    <div className={`${classes.scroll_container} h-full flex flex-row md:flex-col gap-5`}>
                         {heroImages.map((image, index) => (
                             <CourselCard
                                 key={index}
